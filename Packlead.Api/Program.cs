479f88688d8f00dfa391d312299c6a34e26303d9
devdiagon <fredericktimo@gmail.com>
2026-06-28 23:44:30 -0500

    feat: add DB context & service reference

--- Packlead.Api/Program.cs
+++ Packlead.Api/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Packlead.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
