5155162438f86d8354a9683d17d2cc888981ce42
devdiagon <fredericktimo@gmail.com>
2026-06-28 23:41:16 -0500

    feat: add order & dispatcher table definition

---
+++ Packlead.Infrastructure/Persistance/Configurations/DispatcherConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Packlead.Domain.Entities;
+
+namespace Packlead.Infrastructure.Persistence.Configurations;
+
+public class DispatcherConfiguration : IEntityTypeConfiguration<Dispatcher>
+{
+    public void Configure(EntityTypeBuilder<Dispatcher> builder)
+    {
+        builder.ToTable("Dispatchers");
+
+        builder.HasKey(d => d.Id);
+
+        builder.Property(d => d.FirebaseUid)
+            .IsRequired()
+            .HasMaxLength(128);
+
+        builder.HasIndex(d => d.FirebaseUid)
+            .IsUnique();
+
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(d => d.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(d => d.Vehicle)
+            .HasMaxLength(100);
+
+        builder.Property(d => d.LicensePlate)
+            .HasMaxLength(20);
+
+        builder.Property(d => d.State)
+            .HasConversion<string>()
+            .HasMaxLength(20);
+    }
+}
---
+++ Packlead.Infrastructure/Persistance/Configurations/OrderConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Packlead.Domain.Entities;
+
+namespace Packlead.Infrastructure.Persistence.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.ClientName)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(o => o.ClientPhoneNumber)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(o => o.Address)
+            .HasMaxLength(300);
+
+        builder.Property(o => o.Zone)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(o => o.State)
+            .HasConversion<string>()
+            .HasMaxLength(20);
+
+        builder.Property(o => o.DeliveryDate)
+            .IsRequired();
+
+        builder.Property(o => o.CreatedAt)
+            .IsRequired();
+
+        // Location como owned entity: columnas Lat/Lng embebidas en Orders (sin tabla)
+        builder.OwnsOne(o => o.Location, loc =>
+        {
+            loc.Property(l => l.Lat).HasColumnName("Lat").IsRequired();
+            loc.Property(l => l.Lng).HasColumnName("Lng").IsRequired();
+        });
+
+        // FK nullable hacia Dispatcher
+        builder.HasOne<Dispatcher>()
+            .WithMany()
+            .HasForeignKey(o => o.DispatcherId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
