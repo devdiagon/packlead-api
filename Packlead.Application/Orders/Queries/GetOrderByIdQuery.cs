efc096a6bf11909033b1a9904534572b1ec73bad
devdiagon <fredericktimo@gmail.com>
2026-06-29 21:03:49 -0500

    feat: add order basic queries

---
+++ Packlead.Application/Orders/Queries/GetOrderByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Packlead.Application.Common.Interfaces;
+using Packlead.Application.Orders.DTOs;
+
+namespace Packlead.Application.Orders.Queries;
+
+public class GetOrderByIdQuery
+{
+    private readonly IOrderRepository _repository;
+
+    public GetOrderByIdQuery(IOrderRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<OrderResponse?> ExecuteAsync(Guid id, CancellationToken ct = default)
+    {
+        var order = await _repository.GetByIdAsync(id, ct);
+        return order?.ToResponse();
+    }
+}
