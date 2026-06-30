9c9c5bf5c44c6167a8e135f9372273122fc9711e
devdiagon <fredericktimo@gmail.com>
2026-06-30 10:15:35 -0500

    feat: add dispatcher exceptions

---
+++ Packlead.Application/Common/Exceptions/DispatcherExceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Packlead.Application.Common.Exceptions;
+
+public class DispatcherNotFoundException : Exception
+{
+    public DispatcherNotFoundException(Guid id)
+        : base($"Dispatcher with id '{id}' was not found.") { }
+}
+
+public class DuplicateFirebaseUidException : Exception
+{
+    public DuplicateFirebaseUidException(string firebaseUid)
+        : base($"A dispatcher with FirebaseUid '{firebaseUid}' already exists.") { }
+}
