54d0bde2bff58450cf4f1a13f489b565eacb0a3b
devdiagon <fredericktimo@gmail.com>
2026-06-30 10:21:30 -0500

    feat: add dispatcher queries

---
+++ Packlead.Application/Dispatchers/Queries/GetAllDispatchersQuery.cs
@@ -0,0 +1,20 @@
+﻿using Packlead.Application.Common.Interfaces;
+using Packlead.Application.Dispatchers.DTOs;
+
+namespace Packlead.Application.Dispatchers.Queries;
+
+public class GetAllDispatchersQuery
+{
+    private readonly IDispatcherRepository _repository;
+
+    public GetAllDispatchersQuery(IDispatcherRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<DispatcherResponse>> ExecuteAsync()
+    {
+        var dispatchers = await _repository.GetAllAsync();
+        return dispatchers.Select(d => d.ToResponse()).ToList();
+    }
+}
---
+++ Packlead.Application/Dispatchers/Queries/GetDispatcherByIdQuery.cs
@@ -0,0 +1,23 @@
+﻿using Packlead.Application.Common.Exceptions;
+using Packlead.Application.Common.Interfaces;
+using Packlead.Application.Dispatchers.DTOs;
+
+namespace Packlead.Application.Dispatchers.Queries;
+
+public class GetDispatcherByIdQuery
+{
+    private readonly IDispatcherRepository _repository;
+
+    public GetDispatcherByIdQuery(IDispatcherRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<DispatcherResponse> ExecuteAsync(Guid id)
+    {
+        var dispatcher = await _repository.GetByIdAsync(id)
+            ?? throw new DispatcherNotFoundException(id);
+
+        return dispatcher.ToResponse();
+    }
+}
