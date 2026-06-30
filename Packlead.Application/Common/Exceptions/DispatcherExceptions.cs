f30af2cf4fb7850b1b42630a81bced461372c0aa
devdiagon <fredericktimo@gmail.com>
2026-06-30 10:44:31 -0500

    refactor: update dispatcher exceptions with custom generic App exception

--- Packlead.Application/Common/Exceptions/DispatcherExceptions.cs
+++ Packlead.Application/Common/Exceptions/DispatcherExceptions.cs
@@ -1,13 +1,19 @@
-﻿namespace Packlead.Application.Common.Exceptions;
+﻿using Packlead.Application.Common.Exceptions;
 
-public class DispatcherNotFoundException : Exception
+public class DispatcherNotFoundException : AppException
 {
+    public override int StatusCode => 404;
+    public override string ErrorCode => "NotFound";
+
     public DispatcherNotFoundException(Guid id)
         : base($"Dispatcher with id '{id}' was not found.") { }
 }
 
-public class DuplicateFirebaseUidException : Exception
+public class DuplicateFirebaseUidException : AppException
 {
+    public override int StatusCode => 409;
+    public override string ErrorCode => "DuplicateFirebaseUid";
+
     public DuplicateFirebaseUidException(string firebaseUid)
         : base($"A dispatcher with FirebaseUid '{firebaseUid}' already exists.") { }
 }
