3ee863cb261860cd150fab0712047a62ce8e798d
devdiagon <fredericktimo@gmail.com>
2026-07-09 12:53:03 -0500

    feat: specify a new DTO for distpacher request/response
    Made firebaseUID optional on POST request, doesn't make sense to create a dispatcher if the UID doesn't exist yet

--- Packlead.Application/Dispatchers/Commands/CreateDispatcherCommand.cs
+++ Packlead.Application/Dispatchers/Commands/CreateDispatcherCommand.cs
@@ -1,5 +1,4 @@
-﻿using Packlead.Application.Common.Exceptions;
-using Packlead.Application.Common.Interfaces;
+﻿using Packlead.Application.Common.Interfaces;
 using Packlead.Application.Dispatchers.DTOs;
 using Packlead.Domain.Entities;
 
@@ -8,27 +7,52 @@ namespace Packlead.Application.Dispatchers.Commands;
 public class CreateDispatcherCommand
 {
     private readonly IDispatcherRepository _repository;
+    private readonly IFirebaseUserService _firebaseUserService;
 
-    public CreateDispatcherCommand(IDispatcherRepository repository)
+    public CreateDispatcherCommand(IDispatcherRepository repository, IFirebaseUserService firebaseUserService)
     {
         _repository = repository;
+        _firebaseUserService = firebaseUserService;
     }
 
-    public async Task<DispatcherResponse> ExecuteAsync(CreateDispatcherRequest request)
+    public async Task<CreateDispatcherResponse> ExecuteAsync(CreateDispatcherRequest request, CancellationToken ct)
     {
-        if (await _repository.ExistsByFirebaseUidAsync(request.FirebaseUid))
-            throw new DuplicateFirebaseUidException(request.FirebaseUid);
+        var isMigration = request.FirebaseUid is not null;
+        string firebaseUid;
+        string? passwordResetLink = null;
+
+        if (isMigration)
+        {
+            if (await _repository.ExistsByFirebaseUidAsync(request.FirebaseUid!))
+                throw new DuplicateFirebaseUidException(request.FirebaseUid!);
+
+            firebaseUid = request.FirebaseUid!;
+        }
+        else
+        {
+            firebaseUid = await _firebaseUserService.CreateDispatcherUserAsync(request.Email, ct);
+            passwordResetLink = await _firebaseUserService.GeneratePasswordResetLinkAsync(request.Email, ct);
+        }
 
         var dispatcher = new Dispatcher(
-            firebaseUid: request.FirebaseUid,
+            firebaseUid: firebaseUid,
             name: request.Name,
             email: request.Email,
             vehicle: request.Vehicle,
             licensePlate: request.LicensePlate
         );
 
-        await _repository.CreateAsync(dispatcher);
+        try
+        {
+            await _repository.CreateAsync(dispatcher);
+        }
+        catch when (!isMigration)
+        {
+            await _firebaseUserService.DeleteUserAsync(firebaseUid, ct);
+            throw;
+        }
 
-        return dispatcher.ToResponse();
+        var baseResponse = dispatcher.ToResponse();
+        return CreateDispatcherResponse.FromDispatcherResponse(baseResponse, passwordResetLink);
     }
 }
